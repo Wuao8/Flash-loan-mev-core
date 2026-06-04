168bf5413efb65010ff4de62561d46d707f64b02
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-04 15:30:44 +0200

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,1 +1,11 @@
-print("Solana Arbitrage Bot avviato")
+import requests
+
+print("Test connessione Jupiter...")
+
+url = "https://price.jup.ag/v4/price?ids=SOL"
+
+response = requests.get(url)
+
+print("Status Code:", response.status_code)
+print("Risposta API:")
+print(response.text)
