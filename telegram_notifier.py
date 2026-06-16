c93857301fde5c399ea1427b114a70a45cd5b61a
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-16 15:45:01 +0200

    Create telegram_notifier.py

---
+++ telegram_notifier.py
@@ -0,0 +1,19 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+
+def send_telegram(msg: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    try:
+        requests.post(
+            url,
+            json={
+                "chat_id": TELEGRAM_CHAT_ID,
+                "text": msg,
+                "parse_mode": "HTML"
+            },
+            timeout=10
+        )
+    except Exception as e:
+        print("TELEGRAM ERROR:", e)
