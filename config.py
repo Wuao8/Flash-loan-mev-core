3e7ee1b991bea682b334e63800f80f58906ec223
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-16 15:49:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 TELEGRAM_BOT_TOKEN = "8881155593:AAFK7n5nqr4ulahVmmAtQhye3ugzL58cqJI"
 TELEGRAM_CHAT_ID = "7461254501"
+
+# BASE RPC (lo aggiungiamo dopo quando crei Alchemy)
+BASE_RPC_URL = ""
+
+# SETTINGS
+CHAIN = "base"
+
