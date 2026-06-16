c1a62edd8b09b185758e8fd14d8076c52a94f946
Vanya <126008208+Wuao8@users.noreply.github.com>
2026-06-16 16:07:30 +0200

    Create token_list.py

---
+++ scanner/token_list.py
@@ -0,0 +1,9 @@
+TOKENS_BASE = {
+    # esempio token su Base (poi ampliamo con address reali)
+    "ETH": "0x4200000000000000000000000000000000000006",
+    "USDC": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+}
+
+
+def get_tokens():
+    return TOKENS_BASE
